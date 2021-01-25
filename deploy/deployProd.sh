3e0493064770268c5e5f87791ee37887a367a91f
pure <670064326@qq.com>
2021-01-25 18:12:24 +0800

    chore: 部署文件

--- deploy/deployProd.sh
+++ deploy/deployProd.sh
@@ -9,13 +9,13 @@ FILEPATH=/home/note
 #构建成功后打包文件上传到服务器
 # 阿里云
 # 构建成功后打包文件上传到服务器
-cd dist
-tar -zcvf ${DEPLOYFILES} ./* ../favicon.ico
+cd dist/${DEPLOYNAME}
+tar -zcvf ${DEPLOYFILES} ../* ../../favicon.ico
 scp -r ${DEPLOYFILES} root@${SERVER_1}:${FILEPATH}/tarfiles
 rm -rf ./*
 
 # 解压文件
-ssh -o StrictHostKeyChecking=no root@${SERVER_1} tar xzf ${FILEPATH}/tarfiles/${DEPLOYFILES} -C ${FILEPATH}
+ssh -o StrictHostKeyChecking=no root@${SERVER_1} tar xzf ${FILEPATH}/tarfiles/${DEPLOYFILES} -C ${FILEPATH}/${DEPLOYNAME}
 
 if [ $? -ne 0 ]; then
     echo "部署失败"
