4ed99341c3b04f3189d73e7bc41f743c04bf790e
zhangjunrao <zhangjunrao@chicv.com>
2022-04-12 11:19:07 +0800

    chore: md code样式

--- src/containers/components/ReactMarkdown/index.tsx
+++ src/containers/components/ReactMarkdown/index.tsx
@@ -2,7 +2,12 @@ import * as React from 'react'
 import cs from 'classnames'
 
 import _ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import rehypeKatex from 'rehype-katex'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import styles from './index.module.scss'
+import 'katex/dist/katex.min.css'
 
 interface Props {
     className?: string
@@ -12,7 +17,32 @@ interface Props {
 const ReactMarkdown: React.FC<Props> = ({ className, content }: Props) => {
     return (
         <div className={cs(styles.container, className)}>
-            <_ReactMarkdown>{content}</_ReactMarkdown>
+            <_ReactMarkdown
+                remarkPlugins={[[remarkGfm, { singleTilde: false }]]}
+                rehypePlugins={[rehypeKatex]}
+                components={{
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    code({ node, inline, className, children, ...props }) {
+                        const match = /language-(\w+)/.exec(className || '')
+                        return !inline && match ? (
+                            <SyntaxHighlighter
+                                // eslint-disable-next-line react/no-children-prop
+                                children={String(children).replace(/\n$/, '')}
+                                style={dark}
+                                language={match[1]}
+                                PreTag="div"
+                                {...props}
+                            />
+                        ) : (
+                            <code className={className} {...props}>
+                                {children}
+                            </code>
+                        )
+                    },
+                }}
+            >
+                {content}
+            </_ReactMarkdown>
         </div>
     )
 }
