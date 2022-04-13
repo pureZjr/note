f17cbba5b98faea0727598564fd46b2d6d01db0d
zhangjunrao <zhangjunrao@chicv.com>
2022-04-13 16:15:09 +0800

    chore: config

--- vite.config.js
+++ vite.config.js
@@ -2,50 +2,52 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import analyze from 'rollup-plugin-analyzer'
 
-export default defineConfig({
-    plugins: [
-        reactRefresh(),
-        analyze({
-            // 用户分析包的大小 和 数量
-            summaryOnly: true,
-            limit: 10, //
-        }),
-    ],
-    base: './',
-    resolve: {
-        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
-        alias: {
-            '@assets/': '/src/assets/',
-            '@components/': '/src/containers/components/',
-            '@constant/': '/src/constant/',
-            '@shared/': '/src/containers/shared/',
-            '@views/': '/src/containers/views/',
-            '@router/': '/src/router/',
-            '@services/': '/src/services/',
-            '@store/': '/src/store/',
-            '@utils/': '/src/utils/',
-            '@typings/': '/typings/',
-            '@config/': '/config/',
-            '@styles/': '/src/styles/',
-            '@nm/': '/node_modules/',
-        },
-    },
-    build: {
-        outDir: 'build',
-        minify: 'terser', // boolean | 'terser' | 'esbuild'
-    },
-    server: {
-        port: 9000,
-        host: 'localhost',
-        open: '/',
-    },
-    css: {
-        preprocessorOptions: {
-            less: {
-                javascriptEnabled: true,
-                modifyVars: {},
+export default defineConfig((args) => {
+    return {
+        plugins: [
+            reactRefresh(),
+            analyze({
+                // 用户分析包的大小 和 数量
+                summaryOnly: true,
+                limit: 10, //
+            }),
+        ],
+        base: './',
+        resolve: {
+            extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+            alias: {
+                '@assets/': '/src/assets/',
+                '@components/': '/src/containers/components/',
+                '@constant/': '/src/constant/',
+                '@shared/': '/src/containers/shared/',
+                '@views/': '/src/containers/views/',
+                '@router/': '/src/router/',
+                '@services/': '/src/services/',
+                '@store/': '/src/store/',
+                '@utils/': '/src/utils/',
+                '@typings/': '/typings/',
+                '@config/': '/config/',
+                '@styles/': '/src/styles/',
+                '@nm/': '/node_modules/',
             },
         },
-    },
-    define: {},
+        build: {
+            outDir: `build/${args.mode}`,
+            minify: 'terser', // boolean | 'terser' | 'esbuild'
+        },
+        server: {
+            port: 9000,
+            host: 'localhost',
+            open: '/',
+        },
+        css: {
+            preprocessorOptions: {
+                less: {
+                    javascriptEnabled: true,
+                    modifyVars: {},
+                },
+            },
+        },
+        define: {},
+    }
 })
