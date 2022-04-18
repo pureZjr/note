cd636e2d054d6c78895a7d7663a57d0cd84e45f2
zhangjunrao <zhangjunrao@chicv.com>
2022-04-18 18:28:19 +0800

    feat: 没有分享资源页面

--- src/containers/views/ShareArticle/index.tsx
+++ src/containers/views/ShareArticle/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { Input, Avatar, Button } from 'antd'
 import dayjs from 'dayjs'
-import { LikeOutlined, EyeOutlined } from '@ant-design/icons'
+import { LikeOutlined, EyeOutlined, ShareAltOutlined } from '@ant-design/icons'
 
 import Header from '@components/Header'
 import RenderContent from '@shared/RenderContent'
@@ -124,113 +124,123 @@ const ShareArticle: React.FC = () => {
 
     const isLogin = !!userInfo.token
 
-    if (!creator) {
-        return null
-    }
-
     return (
         <div className={styles.container}>
             <Header hideSearch showLogin></Header>
-            <div className={styles.header}>
-                <span className={styles.title}>{title}</span>
-            </div>
-            <div className={styles.content}>
-                <PerfectScroll
-                    style={{
-                        flex: 1,
-                    }}
-                >
-                    <RenderContent type={type} content={content} />
-                </PerfectScroll>
-                <div className={styles.commentWrap}>
-                    <div className={styles.creator}>
-                        <div className={styles.userInfo}>
-                            <Avatar size={30} src={creator.avatar}></Avatar>
-                            <span className={styles.name}>{creator.username}</span>
-                        </div>
-                        <div className={styles.toolbar}>
-                            <div className={styles.read}>
-                                <EyeOutlined width={14} height={14} />
-                                <span>{reads.length}</span>
-                            </div>
-                            <div
-                                className={styles.like}
-                                onClick={like}
-                                style={{ color: liked ? '#3e77ff' : '#898989' }}
-                            >
-                                <LikeOutlined width={14} height={14} />
-                                <span>{likes.length}</span>
-                            </div>
-                        </div>
-                        <div className={styles.createTime}>
-                            <label>分享时间：</label>
-                            <span>{dayjs(createTime).format('YYYY-MM-DD')}</span>
-                        </div>
+            {!!creator ? (
+                <React.Fragment>
+                    <div className={styles.header}>
+                        <span className={styles.title}>{title}</span>
                     </div>
-                    <div className={styles.comments}>
-                        {isLogin ? (
-                            <div className={styles.top}>
-                                <div className={styles.commentLabel}>评论</div>
-                                <Input.TextArea
-                                    className={styles.textarea}
-                                    value={comment}
-                                    onChange={(e) => setComment(e.target.value)}
-                                    onFocus={(e) => {
-                                        e.target.style.height = '80px'
-                                        ;(e.target.nextSibling as HTMLDivElement).style.display = 'flex'
-                                    }}
-                                    onBlur={(e) => {
-                                        const target = e.currentTarget
-                                        setTimeout(() => {
-                                            target.style.height = '40px'
-                                            ;(target.nextSibling as HTMLDivElement).style.display = 'none'
-                                        }, 100)
-                                    }}
-                                />
-                                <div className={styles.btns}>
-                                    <Button onClick={cancel}>取消</Button>
-                                    <Button
-                                        style={{ marginLeft: 7 }}
-                                        type="primary"
-                                        onClick={submit}
-                                        disabled={!comment.length}
+                    <div className={styles.content}>
+                        <PerfectScroll
+                            style={{
+                                flex: 1,
+                            }}
+                        >
+                            <RenderContent type={type} content={content} />
+                        </PerfectScroll>
+                        <div className={styles.commentWrap}>
+                            <div className={styles.creator}>
+                                <div className={styles.userInfo}>
+                                    <Avatar size={30} src={creator.avatar}></Avatar>
+                                    <span className={styles.name}>{creator.username}</span>
+                                </div>
+                                <div className={styles.toolbar}>
+                                    <div className={styles.read}>
+                                        <EyeOutlined width={14} height={14} />
+                                        <span>{reads.length}</span>
+                                    </div>
+                                    <div
+                                        className={styles.like}
+                                        onClick={like}
+                                        style={{ color: liked ? '#3e77ff' : '#898989' }}
                                     >
-                                        评论
-                                    </Button>
+                                        <LikeOutlined width={14} height={14} />
+                                        <span>{likes.length}</span>
+                                    </div>
+                                </div>
+                                <div className={styles.createTime}>
+                                    <label>分享时间：</label>
+                                    <span>{dayjs(createTime).format('YYYY-MM-DD')}</span>
                                 </div>
                             </div>
-                        ) : (
-                            <Button
-                                type="primary"
-                                style={{ width: 104 }}
-                                onClick={() => {
-                                    routerStore.history.push('/login')
-                                }}
-                            >
-                                登录后评论
-                            </Button>
-                        )}
-                        <PerfectScroll style={{ overflow: 'hidden', flex: 1 }} scrollToTop={scrollCommentToTop}>
-                            <div className={styles.commentList}>
-                                {responses.map((r) => {
-                                    return (
-                                        <div className={styles.commentItem} key={r.id}>
-                                            <Avatar src={r?.commenter?.avatar}></Avatar>
-                                            <div className={styles.right}>
-                                                <div className={styles.commentorName}>{r?.commenter?.username}</div>
-                                                <div className={styles.comment}>{r.comment}</div>
-                                                <div className={styles.commentCreateTime}>
-                                                    {dayjs(r.commenter.createTime).format('YYYY-MM-DD hh:mm:ss')}
-                                                </div>
-                                            </div>
+                            <div className={styles.comments}>
+                                {isLogin ? (
+                                    <div className={styles.top}>
+                                        <div className={styles.commentLabel}>评论</div>
+                                        <Input.TextArea
+                                            className={styles.textarea}
+                                            value={comment}
+                                            onChange={(e) => setComment(e.target.value)}
+                                            onFocus={(e) => {
+                                                e.target.style.height = '80px'
+                                                ;(e.target.nextSibling as HTMLDivElement).style.display = 'flex'
+                                            }}
+                                            onBlur={(e) => {
+                                                const target = e.currentTarget
+                                                setTimeout(() => {
+                                                    target.style.height = '40px'
+                                                    ;(target.nextSibling as HTMLDivElement).style.display = 'none'
+                                                }, 100)
+                                            }}
+                                        />
+                                        <div className={styles.btns}>
+                                            <Button onClick={cancel}>取消</Button>
+                                            <Button
+                                                style={{ marginLeft: 7 }}
+                                                type="primary"
+                                                onClick={submit}
+                                                disabled={!comment.length}
+                                            >
+                                                评论
+                                            </Button>
                                         </div>
-                                    )
-                                })}
+                                    </div>
+                                ) : (
+                                    <Button
+                                        type="primary"
+                                        style={{ width: 104 }}
+                                        onClick={() => {
+                                            routerStore.history.push('/login')
+                                        }}
+                                    >
+                                        登录后评论
+                                    </Button>
+                                )}
+                                <PerfectScroll style={{ overflow: 'hidden', flex: 1 }} scrollToTop={scrollCommentToTop}>
+                                    <div className={styles.commentList}>
+                                        {responses.map((r) => {
+                                            return (
+                                                <div className={styles.commentItem} key={r.id}>
+                                                    <Avatar src={r?.commenter?.avatar}></Avatar>
+                                                    <div className={styles.right}>
+                                                        <div className={styles.commentorName}>
+                                                            {r?.commenter?.username}
+                                                        </div>
+                                                        <div className={styles.comment}>{r.comment}</div>
+                                                        <div className={styles.commentCreateTime}>
+                                                            {dayjs(r.commenter.createTime).format(
+                                                                'YYYY-MM-DD hh:mm:ss'
+                                                            )}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            )
+                                        })}
+                                    </div>
+                                </PerfectScroll>
                             </div>
-                        </PerfectScroll>
+                        </div>
                     </div>
+                </React.Fragment>
+            ) : (
+                <div className={styles.noShare}>
+                    <ShareAltOutlined style={{ fontSize: 30 }} />
+                    <h2>无效的分享地址</h2>
+                    <p>对方已经停止分享或该分享不存在</p>
                 </div>
-            </div>
+            )}
         </div>
     )
 }
