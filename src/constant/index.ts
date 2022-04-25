c4df4688d94ca70a7d7a32f3e82e8c316e25725f
zhangjunrao <zhangjunrao@chicv.com>
2022-04-25 18:05:00 +0800

    chore: cdn

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -8,7 +8,7 @@ export const FILE_SIZE_LIMIT = 50 * 1024 * 1024
 // 七牛相关
 export const QN_UPLOAD_URL = 'https://upload-z2.qiniup.com'
 export const QN_BUCKET = 'renjianzahuopu-src'
-export const QN_SOURCE_URL = 'https://src.renjianzahuopu.store'
+export const QN_SOURCE_URL = 'https://cdn-src.renjianzahuopu.store'
 
 export const LOCALSTORAGE = {
     USERINFO: 'userinfo',
